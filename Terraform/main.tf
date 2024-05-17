bd69b01f2ef095e1faba1ed1c49f556e111e090f
Soxna_Ngone_Niang <soxnangone>
2024-05-17 13:27:42 +0200

    jenkins

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,3 +1,4 @@
 provider "kubernetes" {
-config_path = "C:\\Users\\Ngoné NIANG SARR\\.kube\\config"
+  config_context_cluster = "minikube"
+  config_path = "C:\\Users\\Ngoné NIANG SARR\\.kube\\config"
 }
